82186a13dcd8a5c35a35d37f100cfc502a43ae3f
salihkemaloglu <logiwa@DESKTOP-IQH06CH>
2019-09-03 16:48:04 +0300

    basic  design

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,17 @@ import { Switch, TextField } from "@material-ui/core";
 import DatePicker, { registerLocale } from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import { tr } from 'date-fns/esm/locale';
+import {
+  createStyles,
+  fade,
+  Theme,
+  withStyles,
+  makeStyles,
+  createMuiTheme,
+} from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
+import { red } from '@material-ui/core/colors';
+import { ThemeProvider } from '@material-ui/styles';
 registerLocale("tr", tr);
 var avatarTest = require("./avatar3.png");
 var avatarAnonym = require("./avatar2.png");
@@ -12,6 +23,7 @@ const App: React.FC = () => {
   const [percent, setPercent] = useState(80);
   const [completed, setCompleted] = useState(false);
   const [dropzoneStatus, setDropzoneStatus] = useState("upload");
+  const [inputType, setInputType] = useState("success");
   const [state, setState] = useState({
     checkedA: false,
     checkedB: false,
@@ -33,6 +45,11 @@ const App: React.FC = () => {
   };
   const handleChangePublisher = (name: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.value);
+    if (event.target.value.length == 0) {
+      setInputType("error");
+    } else {
+      setInputType("success");
+    }
   };
   const handleChangePublisherEmail = (name: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.value);
@@ -63,7 +80,37 @@ const App: React.FC = () => {
   );
   document.addEventListener('DOMContentLoaded', (event) => {
     (document.getElementById("standard-full-width") as HTMLInputElement).style.color = 'black';
+    (document.getElementById("standard-full-width2") as HTMLInputElement).style.color = 'black';
+
+
   });
+
+  const ValidationTextField = withStyles({
+    root: {
+      '& input:invalid + fieldset': {
+        borderColor: 'red',
+        borderWidth: 2,
+      },
+      '& input:valid:focus + fieldset': {
+        borderLeftWidth: 6,
+        padding: '4px !important', // override inline-style
+      },
+      color: 'red',
+    },
+  })(TextField);
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+      },
+      margin: {
+        margin: theme.spacing(1),
+      },
+    }),
+  );
+  const classes = useStyles();
+
   return (
     <div className="App" style={{ paddingTop: '2%' }}>
       <div style={{ display: completed === false ? 'block' : 'none' }}>
@@ -84,55 +131,67 @@ const App: React.FC = () => {
 
               </div>
               <div className="label-text" >
-                <TextField
-                  id="standard-name"
-                  label="Publisher"
-                  defaultValue="John wick"
-                  onChange={handleChangePublisher('name')}
-                  margin="normal"
-                /><br />
-                <TextField
-                  id="standard-name"
-                  label="Information Email"
-                  defaultValue="john@wick.com"
-                  onChange={handleChangePublisherEmail('name')}
-                  margin="normal"
-                /><br />
-              </div>
-              <div className="pickers_label">
-                <div className="pickers">
-                  <DatePicker
-                    selected={startDate}
-                    onChange={date => setStartDate(date)}
-                    showTimeSelect
-                    locale="tr"
-                    timeFormat="HH:mm"
-                    timeIntervals={15}
-                    timeCaption="Time"
-                    todayButton="Today"
-                    dateFormat="d MMMM yyyy h:mm "
+                <div className="label-text-publisher" >
+                  <TextField
+                    required
+                    id="standard-name"
+                    label="Publisher"
+                    defaultValue="John wick"
+                    onChange={handleChangePublisher('name')}
+                    margin="normal"
                   />
                 </div>
+                <form className={classes.root} noValidate>
+                  <ValidationTextField
+                    className={classes.margin}
+                    label="CSS validation style"
+                    required
+                    variant="outlined"
+                    defaultValue="Success"
+                    id="validation-outlined-input"
+                  /></form><br />
+                <div className="label-text-email">
+                  <TextField
+                    id="standard-name"
+                    label="Information Email"
+                    defaultValue="john@wick.com"
+                    onChange={handleChangePublisherEmail('name')}
+                    margin="normal"
+                  />
+                </div><br />
+              </div>
+              <div className="pickers-label">
                 <TextField
                   disabled
                   id="standard-full-width"
-                  defaultValue="Date of opening to capsule:"
+                  defaultValue="Date of opening:"
                   margin="normal"
                   inputProps={{ 'aria-label': 'bare' }}
                 />
-
               </div>
-
+              <div className="pickers">
+                <DatePicker
+                  selected={startDate}
+                  onChange={date => setStartDate(date)}
+                  showTimeSelect
+                  locale="tr"
+                  timeFormat="HH:mm"
+                  timeIntervals={15}
+                  timeCaption="Time"
+                  todayButton="Today"
+                  dateFormat="d MMMM yyyy h:mm "
+                />
+              </div>
             </div>
             <div style={{ display: state.checkedB === true ? 'block' : 'none' }}>
-              <div style={{ float: "left" }}>
+              <div className="avatar-image" style={{ float: "left" }}>
                 <Image src={avatarAnonym} size='small' circular />
               </div>
               <div style={{ float: "left", marginTop: "3%", marginLeft: "2%", textAlign: "left" }}>
-                <code><p><strong>Publisher: </strong>Anonymous User</p></code><br />
-                <code><p><strong>Information Email: </strong>Anonymous Email</p></code>
+                <code><p style={{ marginTop: "2%" }}><strong>Publisher: </strong>Anonymous User</p></code>
+                <code><p className="email-anonym" style={{ marginTop: "5%" }}><strong>Information Email: </strong>Anonymous Email</p></code>
               </div>
-              <div style={{ float: "right", marginRight: "1%" }}>
+              <div className="anonym-switch" style={{ float: "right", marginRight: "1%" }}>
                 <strong>Anonym:<Switch
                   checked={state.checkedB}
                   onChange={handleChange('checkedB')}
@@ -141,20 +200,27 @@ const App: React.FC = () => {
                   inputProps={{ 'aria-label': 'primary checkbox' }}
                 /></strong>
               </div>
-              <div className="pickers">
-                <strong>Date of opening of the capsule:
-                  <DatePicker
-                    selected={startDate}
-                    onChange={date => setStartDate(date)}
-                    showTimeSelect
-                    locale="tr"
-                    timeFormat="HH:mm"
-                    timeIntervals={15}
-                    timeCaption="Time"
-                    todayButton="Today"
-                    dateFormat="d MMMM yyyy h:mm "
-                  />
-                </strong>
+              <div className="pickers-label-anonym">
+                <TextField
+                  disabled
+                  id="standard-full-width2"
+                  defaultValue="Date of opening:"
+                  margin="normal"
+                  inputProps={{ 'aria-label': 'bare' }}
+                />
+              </div>
+              <div className="pickers-anonym">
+                <DatePicker
+                  selected={startDate}
+                  onChange={date => setStartDate(date)}
+                  showTimeSelect
+                  locale="tr"
+                  timeFormat="HH:mm"
+                  timeIntervals={15}
+                  timeCaption="Time"
+                  todayButton="Today"
+                  dateFormat="d MMMM yyyy h:mm "
+                />
               </div>
             </div>
             <div className="line_crate" />
@@ -171,7 +237,7 @@ const App: React.FC = () => {
           </Segment>
         </div>
 
-        <button style={{ marginTop: '1%' }} className="ui fluid secondary  button" onClick={CreateTimeCapsule}>Send Time Capsule to Future</button>
+        <button style={{ marginTop: '1%' }} className="ui fluid secondary  button" onClick={CreateTimeCapsule}>Create Time Capsule</button>
       </div>
       <div style={{ display: completed === true ? 'block' : 'none' }}>
         <Segment placeholder color="black" style={{ width: '75%', marginLeft: '10%', marginTop: '2%' }} >
