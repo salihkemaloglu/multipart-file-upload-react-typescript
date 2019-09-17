eed98563552ef9fc85c83c7a39f09a00522d5ea0
salihkemaloglu <logiwa@DESKTOP-IQH06CH>
2019-09-17 15:53:55 +0300

    date develope

--- src/App-list.tsx
+++ src/App-list.tsx
@@ -4,6 +4,7 @@ import { Segment, Image, Header, Label } from "semantic-ui-react";
 var avatarAnonym = require("./user.png");
 const AppList: React.FC = () => {
   var dates = new Date();
+
   const [countDown, setCountDown] = useState({
     years: 0,
     days: 0,
@@ -11,39 +12,50 @@ const AppList: React.FC = () => {
     minute: 0,
     second: 0,
   });
-
-  const incomeDates = [] as any;
-  document.addEventListener('DOMContentLoaded', (event) => {
-    for (let index = 0; index < 100; index++) {
-      dates.setDate(dates.getDate() + index);
-      incomeDates.push(dates);
-    }
-    startCountdown();
-  });
+  const [incomeDates, setIncomeDates] = useState<any>([]);
+  const damiData = [] as any;
   function startCountdown() {
     var interval = setInterval(() => {
-      incomeDates.forEach((element: any) => {
-        calculateCountdown(element);
+      damiData=[];
+      damiData.forEach((element: any) => {
+        var date = new Date();
+        date.setFullYear(element.years);
+        date.setDate(element.days);
+        date.setHours(element.hours);
+        date.setMinutes(element.minutes);
+        var remaningTime = calculateCountdown(date);
+      damiData.push(calculateCountdown(date));
+
       });
+      setIncomeDates([]);
+      setIncomeDates(damiData);
       if (countDown.second < 0) {
         clearInterval(interval);
       };
-    }, 1000);
+    }, 60000);
   };
+
+  document.addEventListener('DOMContentLoaded', (event) => {
+    for (let index = 0; index < 100; index++) {
+      dates.setDate(dates.getDate() + 10);
+      dates.setHours(dates.getHours() + 10);
+      dates.setMinutes(dates.getMinutes() + 10);
+      damiData.push(calculateCountdown(dates));
+    }
+    setIncomeDates(damiData);
+    startCountdown();
+  });
   function calculateCountdown(endDate: Date) {
+
     let diff = (Date.parse(new Date(endDate).toString()) - Date.parse(new Date().toString())) / 1000;
-
     // clear countdown when date is reached
-    if (diff <= 0) return false;
-
     const timeLeft = {
       years: 0,
       days: 0,
       hours: 0,
-      minute: 0,
+      minutes: 0,
       second: 0,
     };
-
     // calculate time difference between now and expected date
     if (diff >= (365.25 * 86400)) { // 365.25 * 24 * 60 * 60
       timeLeft.years = Math.floor(diff / (365.25 * 86400));
@@ -58,13 +70,10 @@ const AppList: React.FC = () => {
       diff -= timeLeft.hours * 3600;
     }
     if (diff >= 60) {
-      timeLeft.minute = Math.floor(diff / 60);
-      diff -= timeLeft.minute * 60;
+      timeLeft.minutes = Math.floor(diff / 60);
+      diff -= timeLeft.minutes * 60;
     }
-    timeLeft.second = diff;
-
-    setCountDown({ years: timeLeft.years, days: timeLeft.days, hours: timeLeft.days, minute: timeLeft.minute, second: timeLeft.second });
-
+    setCountDown({ years: timeLeft.years, days: timeLeft.days, hours: timeLeft.days, minute: timeLeft.minutes, second: timeLeft.second });
     return timeLeft;
   }
   function addLeadingZeros(value: any) {
@@ -76,57 +85,59 @@ const AppList: React.FC = () => {
   }
   const item = [] as any;
   const TimeCapsule = (props: any) => {
-    for (let index = 0; index < 100; index++) {
-      item.push(
-        <div className="time-capsule-block" >
-          <Segment placeholder color="green" >
-            <Segment.Group horizontal>
-              <Segment>
-                <div className="avatar-image" style={{ float: "left" }}>
-                  <Image src={avatarAnonym} size='small' />
-                </div>
-              </Segment>
-              <Segment style={{ width: "80%" }} stacked>
-                <div className="publisher-info-anonym">
-                  <p ><strong>Publisher: </strong>Anonymous User</p>
-                  <p className="email-anonym" style={{ marginTop: "7%" }}><strong>File Name: </strong>Anonymous Email</p>
-                  <p className="email-anonym" style={{ marginTop: "7%" }}><strong>File Hash: </strong>043a718774c572bd8a25adbeb1bfcd5c0256ae11cecf9f9c3f925d0e52beaf89</p>
-                </div>
-                <Label color="black" style={{ float: "right" }}>#{index}</Label>
-                <Segment.Group horizontal>
-                  <Segment className="square">
-                    <Header as='h1'  >
-                      {addLeadingZeros(countDown.days)}
-                      <Header.Subheader >{countDown.days === 1 ? 'Day' : 'Days'}</Header.Subheader>
-                    </Header>
-                  </Segment>
-                  <Segment className="square">
-                    <Header as='h1' >
-                      {addLeadingZeros(countDown.hours)}
-                      <Header.Subheader>Hours</Header.Subheader>
-                    </Header>
-                  </Segment>
-                  <Segment className="square">
-                    <Header as='h1' >
-                      {addLeadingZeros(countDown.minute)}
-                      <Header.Subheader>Minute</Header.Subheader>
-                    </Header>
-                  </Segment>
-                  <Segment className="square">
-                    <Header as='h1' >
-                      {addLeadingZeros(countDown.second)}
-                      <Header.Subheader>Second</Header.Subheader>
-                    </Header>
-                  </Segment>
-                </Segment.Group>
-              </Segment>
-            </Segment.Group>
-          </Segment>
-          <br />
-        </div>
-      );
-    }
-    return item;
+    // startCountdown();
+    console.log(incomeDates.length)
+      for (let index = 0; index < incomeDates.length; index++) {
+        item.push(
+          <div className="time-capsule-block" >
+            <Segment placeholder color="green" >
+              <Segment.Group horizontal>
+                <Segment>
+                  <div className="avatar-image" style={{ float: "left" }}>
+                    <Image src={avatarAnonym} size='small' />
+                  </div>
+                </Segment>
+                <Segment style={{ width: "80%" }} stacked>
+                  <div className="publisher-info-anonym">
+                    <p ><strong>Publisher: </strong>Anonymous User</p>
+                    <p className="email-anonym" style={{ marginTop: "7%" }}><strong>File Name: </strong>Anonymous Email</p>
+                    <p className="email-anonym" style={{ marginTop: "7%" }}><strong>File Hash: </strong>043a718774c572bd8a25adbeb1bfcd5c0256ae11cecf9f9c3f925d0e52beaf89</p>
+                  </div>
+                  <Label color="black" style={{ float: "right" }}>#{index}</Label>
+                  <Segment.Group horizontal>
+                    <Segment className="square">
+                      <Header as='h1'  >
+                        {addLeadingZeros(incomeDates[index].years)}
+                        <Header.Subheader >{incomeDates[index].years === 1 ? 'Year' : 'Years'}</Header.Subheader>
+                      </Header>
+                    </Segment>
+                    <Segment className="square">
+                      <Header as='h1'  >
+                        {addLeadingZeros(incomeDates[index].days)}
+                        <Header.Subheader >{incomeDates[index].days === 1 ? 'Day' : 'Days'}</Header.Subheader>
+                      </Header>
+                    </Segment>
+                    <Segment className="square">
+                      <Header as='h1' >
+                        {addLeadingZeros(incomeDates[index].hours)}
+                        <Header.Subheader>Hours</Header.Subheader>
+                      </Header>
+                    </Segment>
+                    <Segment className="square">
+                      <Header as='h1' >
+                        {addLeadingZeros(incomeDates[index].minutes)}
+                        <Header.Subheader>Minute</Header.Subheader>
+                      </Header>
+                    </Segment>
+                  </Segment.Group>
+                </Segment>
+              </Segment.Group>
+            </Segment>
+            <br />
+          </div>
+        );
+      }
+      return item;
   }
   return (
     <div className="App" style={{ paddingTop: '2%' }}>
